8ba306e59cec339787cc044a48d32d08c8af9352
Crango <cristian.gomez0887@gmail.com>
2026-07-14 14:21:42 -0500

    refactor: Simplificar la conexión PDO en la clase Database

--- app/config/database.php
+++ app/config/database.php
@@ -19,10 +19,9 @@ class Database
 
             try {
                 self::$conn = new PDO(
-                    "mysql:host=" . $host . ";dbname=" . $db_name,
+                    "mysql:host={$host};dbname={$db_name}",
                     $username,
-                    $password,
-                    [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]
+                    $password
                 );
                 self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
